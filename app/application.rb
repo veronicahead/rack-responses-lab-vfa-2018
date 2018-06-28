5ca10a3156d01d2cb6eaa53bf5f1e8f2e90e0849
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-28 00:37:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,8 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     time = Time.now
-    hour = time.hour
-    if hour <= 12
+    if time.hour <= 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
